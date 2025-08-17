63b2d2593187040cf3f3586bb136f32f533e937d
Jaime <jaiimecast04@gmail.com>
2025-08-17 14:29:50 -0500

    Contains Duplicate

---
+++ Algorithms/Arrays & Hashing/Contains Duplicate.py
@@ -0,0 +1,45 @@
+'''
+PROBLEM DESCRIPTION
+
+Contains Duplicate
+Easy
+
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+
+Input: nums = [1, 2, 3, 3]
+
+Output: true
+
+Example 2:
+
+Input: nums = [1, 2, 3, 4]
+
+Output: false
+'''
+class Solution:
+    '''
+    EXPLANATION
+
+    What I do is create an empty hashmap
+    Iterate through the list `nums`
+    If the element is not in the hashmap,
+    then I add that element in to and itialize a value of 1
+    If it is in the hashmap,
+    then I return True since it's a duplicate and it exists already
+
+    If nothing returns from the previous for loop,
+    then I return False since it has no duplicates
+    '''
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        '''
+        Taking in a list of integers and return a boolean
+        '''
+        dic = {}
+        for num in nums:
+            if num not in dic:
+                dic[num] = 1
+            else:
+                return True
+        return False
